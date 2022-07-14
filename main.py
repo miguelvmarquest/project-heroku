6a9cf23759276710972ba6e2c6d03dcc4acfb4ea
miguelvmarquest <33660939+miguelvmarquest@users.noreply.github.com>
2022-07-14 19:28:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
@@ -22,7 +22,17 @@ def about_me():
 def test():
     return render_template("base.html")
 
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
 
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 if __name__ == '__main__':
     app.run(port="443", use_reloader=True) 

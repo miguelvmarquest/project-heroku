19a0bae49deae212330914119f48df9b0b74b126
miguelvmarquest <33660939+miguelvmarquest@users.noreply.github.com>
2022-07-14 19:10:55 +0100

    update exercicio

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import datetime
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
-    some_text = "Message from the handler."
+    some_t = "Message from the handler."
     current_year = datetime.datetime.now().year
 
-    return render_template("index.html", some_text=some_text, current_year=current_year)
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_t, current_year=current_year, cities=cities)
 
 @app.route("/about")
 def about_me():
